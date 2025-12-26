e5b8b3763848953ed8a7090295c6696a4e4b721c
Peyal Hasan <peyalhasan143@gmail.com>
2025-12-26 19:49:53 +0600

    Displayed all post on Home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
-import { useAuth } from '../hooks/useAuth';
+import React, { useEffect } from 'react';
+import { useReducer } from 'react';
+import { initialState, postReducer } from '../reducers/PostReducter';
+import useAxios from '../hooks/useAxiosAuthentication'
+import PostList from '../Components/posts/PostList';
+import { actions } from '../actions';
+
 
 const HomePage = () => {
-    const { auth } = useAuth()
+    // const { auth } = useAuth()
+
+    const [state, dispatch] = useReducer(postReducer, initialState)
+
+    const { api } = useAxios();
+
+    useEffect(() => {
+        dispatch({ type: actions.post.DATA_FETCHING });
+
+        const fetchPost = async () => {
+            try {
+                const response = await api.get(`${import.meta.env.VITE_SERVER_BASE_URL}/posts`);
+                if (response.status === 200) {
+                    dispatch({ type: actions.post.DATA_FETCHED, data: response.data })
+                }
+            } catch (error) {
+                dispatch({ type: actions.post.DATA_FETCH_ERROR, error: error.message })
+            }
+        }
+
+        fetchPost()
+
+    }, [])
+
+
+
+    if(state?.loading){
+        return <div>We are working...</div>
+    }
+    if(state?.error){
+        return <div>Error in fatching posts {state?.error?.message} </div>
+    }
+
     return (
         <>
             <div >
-                <h1>Home</h1>
+                <PostList posts={state?.posts} />
             </div>
         </>
     );
